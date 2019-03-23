67eebcf00de9dac1d42a22d003b2f3065296fce0
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2019-03-22 20:15:35 -0400

    Added the new files.

---
+++ client/src/views/WageMap.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import mapImage from '../wagemap.jpg';
+import Datamap from 'datamaps/dist/datamaps.world.min.js';
+import * as d3 from 'd3';
+import CanadaJson from './canada.topo.json';
+
+//<img src={mapImage} alt="Wage Map" />
+class WageMap extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    // Datamaps expect data in format:
+    // { "USA": { "fillColor": "#42a844", numberOfWhatever: 75},
+    //   "FRA": { "fillColor": "#8dc386", numberOfWhatever: 43 } }
+    let dataset = {};
+
+    // We need to colorize every country based on "numberOfWhatever"
+    // colors should be uniq for every value.
+    // For this purpose we create palette(using min/max this.props.data-value)
+    let onlyValues = this.props.data.map(function (obj) { return obj[1]; });
+    let minValue = Math.min.apply(null, onlyValues),
+        maxValue = Math.max.apply(null, onlyValues);
+
+    // create color palette function
+    // color can be whatever you wish
+    let paletteScale = d3.scaleLinear()
+        .domain([minValue, maxValue])
+        .range(["#EFEFFF", "#02386F"]); // blue color
+
+    // fill dataset in appropriate format
+    this.props.data.forEach(function (item) { //
+        // item example value ["USA", 70]
+        let iso = item[0],
+            value = item[1];
+        dataset[iso] = { numberOfThings: value, fillColor: paletteScale(value) };
+    });
+
+    let map = new Datamap({
+        element: document.getElementById('cloropleth_map'),
+        scope: 'canada',
+        geographyConfig: {
+            popupOnHover: true,
+            highlightOnHover: true,
+            borderColor: '#444',
+            highlightBorderWidth: 1,
+            borderWidth: 0.5,
+            dataJson: CanadaJson,
+            popupTemplate: function (geo, data) {
+                // don't show tooltip if country don't present in dataset
+                if (!data) { return; }
+                // tooltip content
+                return ['<div class="hoverinfo">',
+                    '<strong>', geo.properties.name, '</strong>',
+                    '<br>Count: <strong>', data.numberOfThings, '</strong>',
+                    '</div>'].join('');
+            }
+        },
+        fills: {
+            HIGH: '#afafaf',
+            LOW: '#123456',
+            MEDIUM: 'blue',
+            UNKNOWN: 'rgb(0,0,0)',
+            defaultFill: '#eee'
+        },
+        data: dataset,
+        setProjection: function (element) {
+            var projection = d3.geoMercator()
+                .center([-106.3468, 68.1304]) // always in [East Latitude, North Longitude]
+                .scale(200)
+                .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+
+            var path = d3.geoPath()
+                .projection(projection);
+            return { path: path, projection: projection };
+        }
+    });
+  }
+  render() {
+    return (
+      <div className='Wage-Map'>
+        <h1>Welcome to the Wage Visualizer!</h1>
+        <div id="cloropleth_map" style={{height: "100%", width: "100%"}}></div>
+      </div>);
+  }
+}
+
+export default WageMap;
---
+++ server.js
@@ -0,0 +1,149 @@
+const
+	dotenv = require('dotenv').load(),
+	express = require('express'),
+	app = express(),
+	logger = require('morgan'),
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	mongojs = require("mongojs"),
+	csv = require('csvtojson'),
+	MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/wage-visualizer',
+	PORT = process.env.PORT || 3001,
+	usersRoutes = require('./routes/users.js')
+
+mongoose.set('useCreateIndex', true)
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+	console.log(err || `Connected to MongoDB.`)
+})
+
+app.use(express.static(`${__dirname}/client/build`))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/api', (req, res) => {
+	res.json({message: "API root."})
+})
+
+app.use('/api/users', usersRoutes)
+
+app.use('*', (req, res) => {
+	res.sendFile(`${__dirname}/client/build/index.html`)
+})
+
+var databaseUrl = "project3";
+var collections = ["project3data"];
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function (error) {
+    console.log("Database Error:", error);
+});
+
+db.project3data.remove({});
+
+var csvFilePath = './csv/wage file.csv'
+csv()
+    .fromFile(csvFilePath)
+    .then(function (data) {
+        for (i = 0; i < data.length; i++) {
+            db.project3data.save({
+                Country: data[i].Country,
+                Province: data[i].Province,
+                City: data[i].City,
+                Job_Title: data[i].Job_Title,
+                Low_Salary_Yearly: data[i].Low_Salary_Yearly,
+                Median_Salary_Yearly: data[i].Median_Salary_Yearly,
+                Max_Salary_Yearly: data[i].Max_Salary_Yearly,
+                Currency: data[i].Currency,
+                Year: data[i].Year,
+                Cost_of_Living_Yearly: data[i].Cost_of_Living_Yearly
+            }, function (err, inserted) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    // console.log(inserted);
+                }
+            });
+        }
+    })
+
+
+function getMin(inputA, inputB) {
+    app.get("/api/min", function (req, res) {
+        db.project3data.find(
+            { City: inputA, Job_Title: inputB },
+            {
+                Low_Salary_Yearly: 1,
+                _id: 0
+            }, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.send(result);
+                }
+            }
+        )
+    })
+};
+
+function getMax(inputA, inputB) {
+    app.get("/api/max", function (req, res) {
+        db.project3data.find(
+            { City: inputA, Job_Title: inputB },
+            {
+                Max_Salary_Yearly: 1,
+                _id: 0
+            }, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.send(result);
+                }
+            }
+        )
+    })
+};
+
+function getMed(inputA, inputB) {
+    app.get("/api/med", function (req, res) {
+        db.project3data.find(
+            { City: inputA, Job_Title: inputB },
+            {
+                Median_Salary_Yearly: 1,
+                _id: 0
+            }, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.send(result);
+                }
+            }
+        )
+    })
+};
+
+function getCost(inputA, inputB) {
+    app.get("/api/cost", function (req, res) {
+        db.project3data.find(
+            { City: inputA, Job_Title: inputB },
+            {
+                Cost_of_Living_Yearly: 1,
+                _id: 0
+            }, function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    res.send(result);
+                }
+            }
+        )
+    })
+};
+
+app.listen(PORT, (err) => {
+	console.log(err || `Server running on port ${PORT}.`)
+})
