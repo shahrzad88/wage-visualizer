4430d550d35fdbcb70dbf5b5dc735a1ad5a7652f
Shahrzad Masoumnia <shahrzad@spiretrading.com>
2019-03-23 00:21:59 -0400

    last

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	mongojs = require("mongojs"),
-	csv = require('csvtojson'),
+    csv = require('csvtojson'),
 	MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/wage-visualizer',
 	PORT = process.env.PORT || 3001,
 	usersRoutes = require('./routes/users.js')
